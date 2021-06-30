cc317dfed2dcdd06dd1d8df6ec8b7c8e7d8d2159
danielhpt <38636029+danielhpt@users.noreply.github.com>
2021-06-30 02:05:30 +0100

    html

--- project/settings/production.py
+++ project/settings/production.py
@@ -38,6 +38,6 @@ DATABASES = {
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASS'],
         'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
+        'PORT': int(os.environ['DB_PORT']),
     }
 }
