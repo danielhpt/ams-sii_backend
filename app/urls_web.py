169ec71eea2ab128924c04bc95ca899ecfab4792
danielhpt <38636029+danielhpt@users.noreply.github.com>
2021-06-30 03:26:03 +0100

    urls

--- app/urls_web.py
+++ app/urls_web.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
 from django.views.generic.base import TemplateView
-from django.urls import path
+from django.urls import path, include
 
 from . import views_web
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    path('admin/', admin.site.urls),
+    path('api/', include('app.urls_api')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
     path('users/<int:user_id>/', views_web.userDetails, name='user_detail'),
     path('occurrences/<int:occurrence_id>/', views_web.occurrenceDetails, name="occurrence_detail"),
     path('victims/<int:victim_id>/', views_web.victimDetails, name="victim_detail"),
