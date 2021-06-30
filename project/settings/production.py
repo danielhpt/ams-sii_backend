535d1c9baa9bf63eece0055e95c7af72fb9ef1e9
danielhpt <38636029+danielhpt@users.noreply.github.com>
2021-06-30 02:47:55 +0100

    html

--- project/settings/production.py
+++ project/settings/production.py
@@ -2,6 +2,9 @@ import os
 
 from pathlib import Path
 
+
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,6 +71,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
